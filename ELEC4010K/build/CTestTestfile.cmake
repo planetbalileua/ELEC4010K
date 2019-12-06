df0b96d0130e471100892036db69a747b2eec7c6
planetbalileua <nicoley1997@gmail.com>
2019-12-06 09:05:05 +0800

    task1&2

---
+++ ELEC4010K/build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/balileua/catkin_ws/src
+# Build directory: /home/balileua/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(control)
+subdirs(hector_slam)
+subdirs(vrep_ros_interface)
